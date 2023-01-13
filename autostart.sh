a5e6e497aefeb87dfc605edb542a560e66ce8d85
jephdo <jephdo@gmail.com>
2023-01-13 11:48:13 -0800

    Modify autostart script to not rely on bash alias

--- autostart.sh
+++ autostart.sh
@@ -4,5 +4,7 @@ ps -ef | pgrep -fl ghostseeder.py
 if [ $? -eq 1 ] 
 then
     echo "Ghostseeder not running"
-    startghostseed
+    DIR="$(cd "$(dirname "$0")" && pwd)"
+
+    nohup $DIR/env/bin/python $DIR/ghostseeder/ghostseeder.py -f $DIR/torrents/ -p 55570 &>> $DIR/output.log &
 fi
